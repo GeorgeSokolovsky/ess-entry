01705b9cc5e6eeecd55705bef0d10a5bc5464081
George Sokolovsky <georgesokolovsky@gmail.com>
2019-01-25 23:57:17 +0300

    fix init user at req context

--- src/graphQL-config.service.ts
+++ src/graphQL-config.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { GqlOptionsFactory, GqlModuleOptions } from '@nestjs/graphql';
 import { join } from 'path';
 import { UsersService } from '@common/services';
+import { User } from '@common/schema/graphql.schema';
 
 @Injectable()
 export class GraphQLConfigService implements GqlOptionsFactory {
@@ -10,8 +11,15 @@ export class GraphQLConfigService implements GqlOptionsFactory {
   async createGqlOptions(): Promise<GqlModuleOptions> {
     return {
       context: async ({ req }) => {
-        const token = req.headers.authorization || '';
-        const user = await this.userService.verifyToken(token);
+        const header = req.headers.authorization || '';
+        const [, token] = header.split(' ');
+        let user: User;
+
+        try {
+          user = await this.userService.verifyToken(token || '');
+        } catch (e) {
+          user = null;
+        }
 
         return {
           user,
